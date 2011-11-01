70225e1dfb455bd353383087fd1f82ddf7a0eafc
Tanner Rogalsky <the.t.dawg@gmail.com>
2011-11-01 14:12:43 -0400

    clean up config and remove junk

---
+++ config.coffee
@@ -0,0 +1,5 @@
+exports.config =
+  host: 'irc.mountai.net'
+  port: 6667
+  username: 'nodelog'
+  channel: '#hubot-test'
--- config.js
+++
@@ -1,6 +0,0 @@
-exports.config = {
-  host: 'irc.mountai.net',
-  port: 6667,
-  user: 'nodelog',
-  channel: '#hubot-test'
-};
